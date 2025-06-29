02d0950565b24026697d2d0b43471b6e528b9c8b
sailesh818 <sailesh7019@gmail.com>
2025-06-29 17:34:10 +0545

    git code aadded

--- scrap.py
+++ scrap.py
@@ -1,3 +1,23 @@
+
+# go to git bash
+# git config --global user.name "sailesh818"
+# git config --global user.email "sailesh7019@gmail.com"
+
+# git init
+# git status => if you want to check what are the status of files
+# git diff => if you want to check what are the changes 
+# git add .
+# git commit -m "Your messages"
+# copy paste git code from github
+
+
+#########
+# 1. change the code 
+# 2. git add.
+# 3. git commit -m "Your message"
+# 4. git push
+###########
+
 import json
 import csv
 import requests
